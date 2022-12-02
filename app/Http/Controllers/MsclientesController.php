cd3ef93ca3a963c058609e427f7e1d1c1350c80c
luanavm <103612463+luanavm@users.noreply.github.com>
2022-12-02 09:37:55 -0300

    first commit

---
+++ app/Http/Controllers/MsclientesController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\msclientes;
+use Illuminate\Http\Request;
+
+class MsclientesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\msclientes  $msclientes
+     * @return \Illuminate\Http\Response
+     */
+    public function show(msclientes $msclientes)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\msclientes  $msclientes
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(msclientes $msclientes)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\msclientes  $msclientes
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, msclientes $msclientes)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\msclientes  $msclientes
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(msclientes $msclientes)
+    {
+        //
+    }
+}
